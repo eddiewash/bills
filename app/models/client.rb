aca7fdebd48575364abcf109b85daa0a25fba987
Steve <stevemargc@gmail.com>
2012-10-05 16:28:31 -0400

    added simple form and styled forms

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,6 @@
 class Client < ActiveRecord::Base
-  attr_accessible :address1, :address2, :city, :client_name, :notes, :state
+  attr_accessible :user_id, :address1, :address2, :city, :client_name, :notes, :state
+  belongs_to :user
+  
+
 end
