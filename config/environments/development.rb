4b80c96046f83475191c87d26700ae4ec1f109f6
Steve <stevemargc@gmail.com>
2012-10-05 11:15:12 -0400

    Installed devise, added user and installed devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Bills::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #required by devise documentation
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Bills::Application.routes.draw do
+  devise_for :users
+
   resources :clients
+  root to: 'clients#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
