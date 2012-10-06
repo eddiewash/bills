0fd22b5ba72f46ae7b82f08ed0f3aa14c624d4da
Steve <stevemargc@gmail.com>
2012-10-06 17:12:47 -0400

    created custom routes for signin and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Bills::Application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    get "register", to: "devise/registrations#new", as: :register
+    get "signin", to: "devise/sessions#new", as: :signin
+    get "logout", to: "devise/sessions#destroy", as: :logout
+    
+  end
 
   resources :clients
   root to: 'clients#index'
+  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
