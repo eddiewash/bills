3ecefacf0fdfa02a21328fb67bd38f9e21ece473
Steve <stevemargc@gmail.com>
2012-10-06 14:20:35 -0400

    added tests for registration, first and last name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user needs to enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+  
+  test "a user needs to enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  
 end
