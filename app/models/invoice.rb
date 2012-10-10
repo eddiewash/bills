936394c05d5b847d0dd61400ea39a04e163838cf
Steve <stevemargc@gmail.com>
2012-10-09 22:47:57 -0400

    added unit tests for invoice validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,8 @@ class Invoice < ActiveRecord::Base
   attr_accessible :job_name, :notes, :service_date, :client_id
   
   belongs_to :client
+  
+  validates :job_name, presence: true
+  validates :service_date, presence: true
+  
 end
--- test/unit/invoice_test.rb
+++ test/unit/invoice_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class InvoiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "test that an invoice requires a job name" do
+    invoice = Invoice.new
+    assert !invoice.save
+    assert !invoice.errors[:job_name].empty?
+  end
+  
+  test "test that an invoice requires a service date" do
+    invoice = Invoice.new
+    assert !invoice.save
+    assert !invoice.errors[:service_date].empty?
+  end
 end
