4d9eb929f2ea56cde41de73bfe5099ac80599ee6
Steve <stevemargc@gmail.com>
2012-11-08 14:46:11 -0500

    updated controller to call the invoice mailer

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,10 +1,16 @@
 class BillingsController < ApplicationController
   
   def create
+    @invoice = Invoice.find(params[:invoice][:invoice_id])
+    @balance = 0
+    @invoice.items.each do |i|
+      @balance = @balance + (i.quantity * i.cost_per) 
+    end
     @billing = Billing.new(params[:invoice])
 
       if @billing.save
-        redirect_to invoices_path, notice: 'Billing was successfully created.' 
+        InvoiceMailer.send_invoice(@invoice, @balance, current_user).deliver
+        redirect_to invoices_path, notice: 'Email was sent.' 
       else
         redirect_to invoices_path
       end
