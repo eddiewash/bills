5882ac036ebff4bde9147f00ee55450cef124564
Steve <stevemargc@gmail.com>
2012-11-08 19:39:53 -0500

    updated production config file to enable mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,18 @@ Bills::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
@@ -64,4 +76,6 @@ Bills::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.action_mailer.default_url_options = { :host => 'http://billme2.herokuapp.com' }
 end
