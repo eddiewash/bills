9aa958f707c349ef0bafe995aed8a386dec9bf6e
Steve <stevemargc@gmail.com>
2012-11-14 13:23:16 -0500

    ran annotate to document updated company model with payment due field

--- test/unit/company_test.rb
+++ test/unit/company_test.rb
@@ -2,19 +2,20 @@
 #
 # Table name: companies
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  phone      :string(255)
-#  email      :string(255)
-#  address1   :string(255)
-#  address2   :string(255)
-#  city       :string(255)
-#  state      :string(255)
-#  zip        :string(255)
-#  logo       :string(255)
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  phone       :string(255)
+#  email       :string(255)
+#  address1    :string(255)
+#  address2    :string(255)
+#  city        :string(255)
+#  state       :string(255)
+#  zip         :string(255)
+#  logo        :string(255)
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  payment_due :integer
 #
 
 require 'test_helper'
