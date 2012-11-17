4424658bcdd7714104741cb347e8d9262814908b
Steve <stevemargc@gmail.com>
2012-11-17 08:59:59 -0500

    added jquery and classes and ids to invoice form partial to hide and show tax id 1 and 2 depending on situation to include inputs, td and th

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,19 +18,70 @@
 //= require_tree .
 jQuery(function($) {
 
-$(document).ready(function(e) {
-  $('FORM').nestedFields({ 
-	containerSelector: 'table',
-	itemSelector: 'tr'
+
+$('FORM').nestedFields({
 	
-	}
+		
+			afterInsert: function() {
+				
+				if (!$("div#tax2 input").val()) {
+			$("td.tax2").hide()
+	  };
+		if (!$("div#tax1 input").val()) {
+	$("td.tax1").hide()
+};
 	
-	);
-});
+	}, 
+  containerSelector: 'table',
+  itemSelector: 'tr',
+}		
+);
+
+
+
+
 
 $("tr[data-link]").click(function() {
   window.location = this.dataset.link
 });
+
+// jquery for hiding and displaying tax
+
+
+if ( !$("div#tax2 input").val()) {
+	$("div#tax2").hide();	
+	$("th.tax2").hide();
+	$("td.tax2").hide();
+}
+else {$("div#tax1_links").hide();}
+
+
+
+$("a#addTax").click(function(){
+	event.preventDefault();
+	$("div#tax2").show();
+	$("th.tax2").show();
+	$("td.tax2").show();
+	$("div#tax1_links").hide();
+});
+$("#removeTax2").click(function(){
+	event.preventDefault();
+	$("div#tax2").hide();
+	$("th.tax2").hide();
+	$("td.tax2").hide();
+	$("input.tax2").prop("checked", false);
+	$("div#tax2 input").val(null);
+	$("div#tax1_links").show();
+});
+$("#removeTax1").click(function(){
+	event.preventDefault();
+	$("th.tax1").hide();
+	$("td.tax1").hide();
+	$("input.tax1").prop("checked", false);
+	$("div#tax1 input").val(0)
+
+});
+
 
 
 })
