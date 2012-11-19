12f29677d1bc7d64efafa66fe38ae9aa9107faf5
Steve <stevemargc@gmail.com>
2012-11-19 14:35:29 -0500

    updated the billings controller to remove @balance since now maintained in invoice table no need to calc balance and pass to invoice mailer

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,15 +1,13 @@
 class BillingsController < ApplicationController
   
   def create
+ 
     @invoice = Invoice.find(params[:invoice_id])
-    @balance = 0
-    @invoice.items.each do |i|
-      @balance = @balance + (i.quantity * i.cost_per) 
-    end
+
     @billing = Billing.new(:invoice_id => params[:invoice_id])
 
       if @billing.save
-        InvoiceMailer.send_invoice(@invoice, @balance, current_user).deliver
+        InvoiceMailer.send_invoice(@invoice, current_user).deliver
         redirect_to invoices_path, notice: 'Email was sent.' 
       else
         redirect_to invoices_path
