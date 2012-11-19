faa9c37dffebf6188058634605fc245b68d868f5
Steve <stevemargc@gmail.com>
2012-11-19 16:45:02 -0500

    added payments model

---
+++ app/models/payment.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id           :integer          not null, primary key
+#  invoice_id   :integer
+#  payment_date :date
+#  amount       :decimal(, )
+#  note         :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Payment < ActiveRecord::Base
+  attr_accessible :amount, :note, :payment_date, :invoice_id
+  
+  belongs_to :invoice
+  
+  validates :amount, presence: true
+  validates :amount, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:less_than => 100000}    
+  
+end
