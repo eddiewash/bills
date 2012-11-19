2f075b2a1cf73bfca8f3944c56cbb4ba69b3910c
Steve <stevemargc@gmail.com>
2012-11-19 18:45:01 -0500

    added show, edit and update actions to the payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,16 +1,11 @@
 class PaymentsController < ApplicationController
   
   def new
- 
     @invoice = Invoice.find(params[:invoice_id])
     @payment = Payment.new
   end
-  
-  
-  
-  
+    
   def create
-
     @payment = Payment.new(params[:payment])
     @payment.invoice_id = params[:invoice_id]
 
@@ -19,6 +14,25 @@ class PaymentsController < ApplicationController
       else
         render :new
       end
+  end
+  
+  def show
+    @invoice = Invoice.find(params[:invoice_id])
+    @payments = @invoice.payments  
+  end
+  
+  def edit
+    @payment = Payment.find(params[:invoice_id])
+  end
+  
+  def update
+    invoice = Invoice.find(params[:id])
+    @payment = Payment.find(params[:invoice_id])
+    if @payment.update_attributes(params[:payment])
+      redirect_to invoice_payment_path(invoice), notice: 'Payment was successfully updated.'
+    else
+      render :edit
+    end   
   end
 
 end
