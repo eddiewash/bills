c83746f562d342803ec4d3042a93b60a48acdeb5
Steve <stevemargc@gmail.com>
2012-11-20 14:03:03 -0500

    updated email controller to send email following save

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -9,10 +9,12 @@ class EmailsController < ApplicationController
   end
   
   def create
+    @invoice = Invoice.find(params[:invoice_id])
     @email = Email.new(params[:email])
     @email.invoice_id = params[:invoice_id]
 
       if @email.save
+        InvoiceMailer.send_invoice(@invoice, @email, current_user).deliver
         redirect_to invoice_path(@email.invoice_id), notice: 'Email was posted.' 
       else
         render :new
