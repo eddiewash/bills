1d4940dbfe3d2b92208e6dbcd7b2c653d833684a
Steve <stevemargc@gmail.com>
2012-11-19 20:19:48 -0500

    added an invoice helper to calulate the account balance

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,11 @@
 module InvoicesHelper
+  
+  def balance(invoice)
+    balance = invoice.total
+    invoice.payments.each do |p|
+      balance = balance - p.amount
+    end   
+    return balance
+  end
+  
 end
