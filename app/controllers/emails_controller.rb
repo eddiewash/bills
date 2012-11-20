f5b3c2921e064f2adfa897e854e7a06aa5527c20
Steve <stevemargc@gmail.com>
2012-11-20 16:08:32 -0500

    updated email controller to set invoice_date during new so could be added to email message, and updated create action so invoice_date would be saved to invoice if emai successful saved and sent

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,6 +3,9 @@ class EmailsController < ApplicationController
   
   def new
     @invoice = Invoice.find(params[:invoice_id])
+    if !@invoice.invoice_date?
+      @invoice.invoice_date = Date.today
+    end
     @email = Email.new
     @email.subject = email_subject(@invoice)
     @email.message = email_message(@invoice)
@@ -12,13 +15,17 @@ class EmailsController < ApplicationController
     @invoice = Invoice.find(params[:invoice_id])
     @email = Email.new(params[:email])
     @email.invoice_id = params[:invoice_id]
+    if !@invoice.invoice_date?
+      @invoice.invoice_date = Date.today
+    end
 
-      if @email.save
-        InvoiceMailer.send_invoice(@invoice, @email, current_user).deliver
-        redirect_to invoice_path(@email.invoice_id), notice: 'Email was posted.' 
-      else
-        render :new
-      end
+    if @email.save
+      InvoiceMailer.send_invoice(@invoice, @email, current_user).deliver
+      @invoice.save
+      redirect_to invoice_path(@email.invoice_id), notice: 'Email was posted.' 
+    else
+      render :new
+    end
   end
 
 end
