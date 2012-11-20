9938377f9bbc9d9d3b4de439c911b2bd411117c1
Steve <stevemargc@gmail.com>
2012-11-20 14:05:08 -0500

    updated invoice mailer to user subject from email table and changed method to accept email instance variable

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,17 +1,14 @@
 class InvoiceMailer < ActionMailer::Base
   default from: "steve@relayhr.com"
 
-  def send_invoice(invoice, current_user)
+  def send_invoice(invoice, email, current_user)
     @invoice = invoice
-    if @invoice.client.poc?
-      @greeting = @invoice.client.poc
-    else
-      @greeting = @invoice.client.client_name
-    end
+    @email = email
+
     
     pdf = InvoicePdf.new(@invoice, view_context)
     attachments["invoice.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render } 
 
-    mail to: @invoice.client.email, subject: "Here is your invoice"
+    mail to: @invoice.client.email, subject: email.subject
   end
 end
