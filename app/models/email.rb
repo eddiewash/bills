5413920d1c2bed0047be71b2a7637bd6c32ab261
Steve <stevemargc@gmail.com>
2012-11-20 11:29:36 -0500

    created email model

---
+++ app/models/email.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: emails
+#
+#  id         :integer          not null, primary key
+#  invoice_id :integer
+#  subject    :text
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Email < ActiveRecord::Base
+  attr_accessible :subject, :message, :invoice_id
+  
+  belongs_to :invoice
+   
+end
