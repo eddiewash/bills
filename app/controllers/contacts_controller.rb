8ddd2c403badfb19de05d4d1e9f0d82b1298b8b9
Steve <stevemargc@gmail.com>
2012-11-21 11:26:13 -0500

    added a contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,48 @@
+class ContactsController < ApplicationController
+  
+  def index
+    @client = Client.find(params[:client_id])
+    @contacts = @client.contacts.all
+  end
+   
+  def new
+    @client = Client.find(params[:client_id])
+    @contact = Contact.new
+  end
+    
+  def create
+    @client = Client.find(params[:client_id])
+    @contact = Contact.new(params[:contact])
+    @contact.client_id = params[:client_id]
+
+    if @contact.save
+      redirect_to client_contact_path, notice: 'Contact was added.' 
+    else
+      render :new
+    end
+
+  end
+  
+  def show
+    @client = Client.find(params[:client_id])
+    @contact = @client.contacts.find(params[:id])
+  end
+  
+  def edit
+    @client = Client.find(params[:client_id])
+    @contact = @client.contacts.find(params[:id])    
+
+  end
+  
+  def update
+    @client = Client.find(params[:client_id])
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(params[:contact])
+      redirect_to client_contact_path(@client, @contact), notice: 'Contact was successfully updated.'
+    else
+      render :edit
+    end
+    
+  end
+  
+end
