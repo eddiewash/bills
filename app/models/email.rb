f5e8c00a0264ef7a554f41aff14a4e58355ba40b
Steve <stevemargc@gmail.com>
2012-11-23 09:16:36 -0500

    updated the email model to accept nested attributes for email_recipients

--- app/models/email.rb
+++ app/models/email.rb
@@ -11,9 +11,11 @@
 #
 
 class Email < ActiveRecord::Base
-  attr_accessible :subject, :message, :invoice_id
+  attr_accessible :subject, :message, :invoice_id, :email_recipients_attributes
   
   belongs_to :invoice
   has_many :email_recipients
+  
+  accepts_nested_attributes_for :email_recipients
    
 end
