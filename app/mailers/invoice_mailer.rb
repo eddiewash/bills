393b7150c03b82084a77bd28c7564f687970e698
Steve <stevemargc@gmail.com>
2012-11-24 10:01:03 -0500

    updated the invoice mailer to accept the recipient and use the email for the receipient as the to address

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,14 +1,12 @@
 class InvoiceMailer < ActionMailer::Base
   default from: "steve@relayhr.com"
 
-  def send_invoice(invoice, email, current_user)
+  def send_invoice(recipient, invoice, email, current_user)
     @invoice = invoice
     @email = email
-
-    
+    @recipient = recipient  
     pdf = InvoicePdf.new(@invoice, view_context)
     attachments["invoice.pdf"] = { :mime_type => 'application/pdf', :content => pdf.render } 
-
-    mail to: @invoice.client.email, subject: email.subject
+    mail to: @recipient.email, subject: @email.subject
   end
 end
