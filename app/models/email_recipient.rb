6ca2d0e70a2d1b60377bfbe5fbaf00c5ddf92310
Steve <stevemargc@gmail.com>
2012-11-24 20:40:46 -0500

    ran annotate to reflect current emailrecipient model.

--- app/models/email_recipient.rb
+++ app/models/email_recipient.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: email_recipients
+#
+#  id         :integer          not null, primary key
+#  email_id   :integer
+#  contact_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class EmailRecipient < ActiveRecord::Base
   attr_accessible :contact_id, :email_id
   
