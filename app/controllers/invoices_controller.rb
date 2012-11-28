280bd5d524b9d7c5683cc567a962ae7e23fc0039
Steve <stevemargc@gmail.com>
2012-11-28 15:45:21 -0500

    added a default payment term for invoice if company value is nil

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -29,7 +29,12 @@ class InvoicesController < ApplicationController
   # GET /invoices/new.json
   def new
     @invoice = Invoice.new
-    @invoice.payment_terms = current_user.company.payment_due
+    if current_user.company.payment_due
+      @invoice.payment_terms = current_user.company.payment_due
+    else
+      @invoice.payment_terms = 0
+    end
+      
     @invoice.tax1 = current_user.company.tax1
     @invoice.tax2 = current_user.company.tax2
     
