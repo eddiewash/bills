0f65b278ee0273bcdab81163c8251566fcc29bbc
Steve <stevemargc@gmail.com>
2012-11-28 15:21:31 -0500

    corrected if statement in email subject helper to return the correct subject

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -3,9 +3,9 @@ module EmailsHelper
   
   def email_subject(invoice, type)
     
-    if type = "thanks"
+    if type == "thanks"
       subject = "Thanks for your business"
-    elsif type = "send_invoice"
+    elsif type == "send_invoice"
       subject = "Invoice: #{invoice.job_name}"
     end
     return subject    
@@ -16,9 +16,7 @@ module EmailsHelper
     if invoice.po_number?
       message = "PO: #{invoice.po_number}\n"
     end
-    
-    
-    
+        
     if type == "thanks"
       message =  message + "Amount: #{number_to_currency(invoice.total)}\nInvoice Date: #{invoice.invoice_date}\nTotal Payments: #{number_to_currency(invoice.total_payments)}\nBalance: #{number_to_currency(invoice.balance)}\n\nThank you for your business. "
     elsif type == "send_invoice"
