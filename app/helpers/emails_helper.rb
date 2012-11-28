492e8c4f77060034a5f1fdd3b0d8bf79f22ccb6a
Steve <stevemargc@gmail.com>
2012-11-28 14:20:52 -0500

    updated the email message helper to create unique message based on email type

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -11,8 +11,19 @@ module EmailsHelper
     return subject    
   end
   
-  def email_message(invoice)
-    message = "PO: #{invoice.po_number}\nAmount: #{number_to_currency(invoice.total)}\nInvoice Date: #{invoice.invoice_date}\nDue Date: #{invoice.due_date} "
+  def email_message(invoice, type)
+    message = ""
+    if invoice.po_number?
+      message = "PO: #{invoice.po_number}\n"
+    end
+    
+    
+    
+    if type == "thanks"
+      message =  message + "Amount: #{number_to_currency(invoice.total)}\nInvoice Date: #{invoice.invoice_date}\nTotal Payments: #{number_to_currency(invoice.total_payments)}\nBalance: #{number_to_currency(invoice.balance)}\n\nThank you for your business. "
+    elsif type == "send_invoice"
+      message = message + "Amount: #{number_to_currency(invoice.total)}\nInvoice Date: #{invoice.invoice_date}\nDue Date: #{invoice.due_date} "
+    end
     return message
   end
 end
