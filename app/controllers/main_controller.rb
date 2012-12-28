6be3a423e477dc57404e3bfabc721b2650d74b12
Steve <stevemargc@gmail.com>
2012-12-27 21:17:12 -0500

    added appointment time as secondary sort on dashboard and appointments index page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
     @search = current_user.clients.search(params[:q])
     @clients = @search.result
     invoices = current_user.invoices
-    @scheduled_appointments = invoices.where("(invoices.invoice_date IS NULL or invoices.balance != ?) and invoices.appointment_date IS NOT NULL", 0).order("appointment_date ASC")
+    @scheduled_appointments = invoices.where("(invoices.invoice_date IS NULL or invoices.balance != ?) and invoices.appointment_date IS NOT NULL", 0).order("appointment_date ASC, appointment_time ASC")
     @open_invoices = invoices.where("invoices.invoice_date IS NULL or invoices.balance != ?", 0)
     
   end
